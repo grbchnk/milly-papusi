c4a77241dd35e36141f182b72968b3a9bed0588b
Stas Gorbachenko <stsrnbw@gmail.com>
2023-09-06 18:17:36 +0300

    fix: button

--- js/scroll.js
+++ js/scroll.js
@@ -40,3 +40,50 @@ window.addEventListener("mousemove", function (e) {
       `px 20px 0px rgba(0, 0, 0, 0.35)`
   })
 })
+
+let smilies = [
+  "\uD83D\uDE18",
+  "\uD83C\uDF52",
+  "\uD83E\uDD70",
+  "\u2764\uFE0F",
+  "\uD83D\uDC96",
+  "\uD83D\uDE3B",
+  "\uD83D\uDC8B",
+]
+let imag2 = document.querySelectorAll(".gallery__img")
+
+imag2.forEach(function (image) {
+  image.addEventListener("click", function (event) {
+    let smiley = document.createElement("span")
+    smiley.textContent = randomSmiley()
+    smiley.style.top =
+      randomPosition(window.innerHeight, smiley.clientHeight) + "px"
+    smiley.style.left =
+      randomPosition(window.innerWidth, smiley.clientWidth) + "px"
+    smiley.style.fontSize = randomSize() + "px"
+    smiley.classList.add("smiley")
+
+    document.body.appendChild(smiley)
+
+    setTimeout(function () {
+      smiley.classList.add("fade-out")
+
+      setTimeout(function () {
+        smiley.remove()
+      }, 500)
+    }, 1000)
+  })
+})
+
+function randomSmiley() {
+  let index = Math.floor(Math.random() * smilies.length)
+  return smilies[index]
+}
+
+function randomPosition(max, size) {
+  return Math.random() * (max - size)
+}
+
+function randomSize() {
+  return Math.floor(Math.random() * 50) + 20
+}
